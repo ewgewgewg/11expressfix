413b5f9401237bce0b66d5e5f70430f93ffee8f1
David Petri <petridw@gmail.com>
2015-10-28 10:30:23 -0700

    (feat) Add wise messages from Berenstains

--- data/messages.js
+++ data/messages.js
@@ -1,14 +1,18 @@
 module.exports = [
   {
-    message: "TEST TEST",
-    created_by: "me"
+    message: "When a problem at school is kept secret too long, it can grow till a cub thinks that everything's wrong.",
+    created_by: "Brother Bear"
   },
   {
-    message: "TEST TEST",
-    created_by: "me"
+    message: "When someone says, 'No, you're too samll for the team', practice and patience may help realize your dream.",
+    created_by: "Sister Bear"
   },
   {
-    message: "TEST TEST",
-    created_by: "me"
+    message: "When cubs and their parents get a little too busy...",
+    created_by: "Mama Bear"
+  },
+  {
+    message: "Their everyday lives get a little too dizzy.",
+    created_by: "Papa Bear"
   }
 ];
