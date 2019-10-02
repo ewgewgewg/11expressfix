e94077f73b6bf630803c371532eaa2feec609c15
Edward Greenberg <ed.w.greenberg@gmail.com>
2019-10-02 13:40:12 -0400

    stuff

--- server/messages/messageController.js
+++ server/messages/messageController.js
@@ -1,12 +1,27 @@
 const messages = require('./../../data/messages');
+const bodyParser = require('body-parser');
+//const inter = messages;
+//let obj = [];
+let i = 0;
 
 module.exports = {
   getMessages: (request,response) => {
     // write code here
-
+    response.send(messages);
+ 
   },
   postMessage: (request,response) => {
     // write code here
+    //messages = messages.concat()
+    //let obj = {message : request.text.message, created_by : request.text.created_by}
+    // console.log(request.text);
+    // inter.push(request.text);
+    console.log(request.body);
+    console.log(messages);
+    messages.push(request.body);
+    //console.log("hi", JSON.parse(response.body), "ho");
+    console.log(i);
+    i++;
 
   }
 };
--- server/server.js
+++ server/server.js
@@ -4,14 +4,28 @@ const path = require('path');
 const messageController = require('./messages/messageController');
 const authController = require('./utils/authController');
 const bodyParser = require('body-parser');
+const success = require('./utils/success');
+const error = require('./utils/error');
+const denied = require('./utils/denied');
 
 app.use(express.static(path.join(__dirname, './../client')));
+app.use(bodyParser.json());
 
 // place routes here
+app.get('/messages',messageController.getMessages);
 
 
-
-
+app.post('/messages',function(req,res,next){
+    //console.log(req.body);
+    if (typeof req.body.message === 'string' && typeof req.body.created_by === 'string'){
+        if (req.headers.authorization !== 'Basic secret_key') {
+            res.send(denied);
+         } else {res.send(success);
+                next();}
+    } else {
+        res.status(400).send(error);
+    }
+},messageController.postMessage);
 
 app.listen(3000);
 
